e677c203c66a0bbe254330b4240e8f438269eb65
ken <yash20004x17@gmail.com>
2025-08-29 09:14:23 +0000

    Built a basic prototype version of ChatApp with the Room Logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,55 @@ import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [skt, setSkt] = useState<WebSocket>();
   const inputRef = useRef(null);
+  const wsRef = useRef<WebSocket | null>(null);
+  const [allMessages, setAllMessages] = useState<string[]>([]);
   const sendMsg = () => {
     // @ts-ignore
     const inputVal = inputRef.current?.value;
     console.log(inputVal);
-    
-    skt?.send(inputVal);
+    wsRef.current?.send(
+      JSON.stringify({
+        type: "chat",
+        payload: {
+          name: "ken",
+          message: `${inputVal}`,
+        },
+      })
+    );
   };
 
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:8080");
-    setSkt(ws);
+    wsRef.current = ws;
+
+    ws.onopen = () => {
+      ws.send(
+        JSON.stringify({
+          type: "join",
+          payload: {
+            name: "ken",
+            roomId: "red",
+          },
+        })
+      );
+    };
     ws.onmessage = (ev) => {
       console.log(ev.data);
+      setAllMessages((mes) => [...mes, ev.data]);
+    };
+    return () => {
+      ws.close();
     };
   }, []);
   return (
     <div className="h-screen w-screen bg-[#212121] text-white flex items-center justify-center">
       <div className="border border-white flex flex-col items-center justify-center">
-        <div className="w-full h-auto p-8"></div>
+        <div className="w-full h-9/10 p-8 overflow-y-auto bg-black text-white">
+          {allMessages.map((m) => (
+            <div className="bg-white text-black px-4 py-2 rounded">{m}</div>
+          ))}
+        </div>
         <div className="flex items-center justify-center p-2">
           <input
             ref={inputRef}
@@ -30,7 +58,10 @@ function App() {
             placeholder="Type message here !!"
             className="bg-[#cccccc] border-none rounded-md px-2 py-1 text-black focus-within:outline-1 outline-[#b1b1b1]"
           />
-          <button className="bg-blue-500 px-2 py-1 rounded-md cursor-pointer font-medium font-mono ml-2  text-[#1a1a1a]" onClick={sendMsg}>
+          <button
+            className="bg-blue-500 px-2 py-1 rounded-md cursor-pointer font-medium font-mono ml-2  text-[#1a1a1a]"
+            onClick={sendMsg}
+          >
             Send
           </button>
         </div>
