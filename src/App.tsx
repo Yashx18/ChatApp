ee6bd2af4793a475334ad070fe6280fb08907829
ken <yash20004x17@gmail.com>
2025-09-09 04:58:06 +0000

    working on New Frontend

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,32 @@ function App() {
   const inputRef = useRef(null);
   const wsRef = useRef<WebSocket | null>(null);
   const [allMessages, setAllMessages] = useState<string[]>([]);
+
+  const roomRef = useRef<any>(null);
+  const usernameRef = useRef<any>(null);
+  const joinRoom = () => {
+    const roomVal = roomRef.current?.value;
+    const usernameVal = usernameRef.current?.value;
+    const ws = wsRef.current;
+    console.log(roomVal, usernameVal);
+        ws.onopen = () => {
+          ws.send(
+            JSON.stringify({
+              type: "join",
+              payload: {
+                name: "ken",
+                roomId: "red",
+              },
+            })
+          );
+        };
+        ws.onmessage = (ev) => {
+          console.log(ev.data);
+          setAllMessages((mes) => [...mes, ev.data]);
+        };
+
+    
+  }
   const sendMsg = () => {
     // @ts-ignore
     const inputVal = inputRef.current?.value;
@@ -43,26 +69,31 @@ function App() {
       ws.close();
     };
   }, []);
+
   return (
-    <div className="h-screen w-screen bg-[#212121] text-white flex items-center justify-center">
-      <div className="border border-white flex flex-col items-center justify-center">
-        <div className="w-full h-9/10 p-8 overflow-y-auto bg-black text-white">
-          {allMessages.map((m) => (
-            <div className="bg-white text-black px-4 py-2 rounded">{m}</div>
-          ))}
-        </div>
-        <div className="flex items-center justify-center p-2">
+    <div className="bg-[#0a0a0a] w-screen h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center border-white w-full max-w-200 border h-auto text-white">
+        <h1 className="text-4xl font-semibold mb-4">Chat Buddy</h1>
+        <div className="w-full max-w-100 flex flex-col items-center justify-center">
           <input
-            ref={inputRef}
             type="text"
-            placeholder="Type message here !!"
-            className="bg-[#cccccc] border-none rounded-md px-2 py-1 text-black focus-within:outline-1 outline-[#b1b1b1]"
+            ref={roomRef}
+            className="bg-[#000000] border px-4 py-2 border-white rounded-md mb-2 w-full focus-within:outline-none"
+            placeholder="Room Code"
           />
+          <input
+            type="text"
+            ref={usernameRef}
+            className="bg-[#000000] border px-4 py-2 border-white rounded-md focus-within:outline-none w-full"
+            placeholder="Username"
+          />
+
           <button
-            className="bg-blue-500 px-2 py-1 rounded-md cursor-pointer font-medium font-mono ml-2  text-[#1a1a1a]"
-            onClick={sendMsg}
+            type="submit"
+            className="bg-white text-black w-full py-2 mt-2 rounded-md cursor-pointer hover:bg-[#e7e7e7] font-medium"
+            onClick={joinRoom}
           >
-            Send
+            Join Room
           </button>
         </div>
       </div>
