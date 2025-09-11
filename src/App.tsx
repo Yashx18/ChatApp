a85965770ec9ae03bc92799bee0be83169e23304
ken <yash20004x17@gmail.com>
2025-09-11 12:09:56 +0000

    Completed HomePage UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const API_URL = import.meta.env.VITE_API_URL;
   const inputRef = useRef(null);
   const wsRef = useRef<WebSocket | null>(null);
   const [allMessages, setAllMessages] = useState<string[]>([]);
@@ -13,24 +16,24 @@ function App() {
     const usernameVal = usernameRef.current?.value;
     const ws = wsRef.current;
     console.log(roomVal, usernameVal);
-        ws.onopen = () => {
-          ws.send(
-            JSON.stringify({
-              type: "join",
-              payload: {
-                name: "ken",
-                roomId: "red",
-              },
-            })
-          );
-        };
-        ws.onmessage = (ev) => {
-          console.log(ev.data);
-          setAllMessages((mes) => [...mes, ev.data]);
-        };
-
-    
-  }
+    if (ws) {
+      ws.onopen = () => {
+        ws.send(
+          JSON.stringify({
+            type: "join",
+            payload: {
+              name: "ken",
+              roomId: "red",
+            },
+          })
+        );
+      };
+      ws.onmessage = (ev) => {
+        console.log(ev.data);
+        setAllMessages((mes) => [...mes, ev.data]);
+      };
+    }
+  };
   const sendMsg = () => {
     // @ts-ignore
     const inputVal = inputRef.current?.value;
@@ -47,7 +50,7 @@ function App() {
   };
 
   useEffect(() => {
-    const ws = new WebSocket("ws://localhost:8080");
+    const ws = new WebSocket(API_URL);
     wsRef.current = ws;
 
     ws.onopen = () => {
@@ -72,31 +75,10 @@ function App() {
 
   return (
     <div className="bg-[#0a0a0a] w-screen h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center justify-center border-white w-full max-w-200 border h-auto text-white">
-        <h1 className="text-4xl font-semibold mb-4">Chat Buddy</h1>
-        <div className="w-full max-w-100 flex flex-col items-center justify-center">
-          <input
-            type="text"
-            ref={roomRef}
-            className="bg-[#000000] border px-4 py-2 border-white rounded-md mb-2 w-full focus-within:outline-none"
-            placeholder="Room Code"
-          />
-          <input
-            type="text"
-            ref={usernameRef}
-            className="bg-[#000000] border px-4 py-2 border-white rounded-md focus-within:outline-none w-full"
-            placeholder="Username"
-          />
-
-          <button
-            type="submit"
-            className="bg-white text-black w-full py-2 mt-2 rounded-md cursor-pointer hover:bg-[#e7e7e7] font-medium"
-            onClick={joinRoom}
-          >
-            Join Room
-          </button>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/home" element={<Home/>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,105 @@
+import { useRef, useState } from "react";
+
+const Home = () => {
+  const inputRef = useRef(null);
+  const wsRef = useRef<WebSocket | null>(null);
+  const [allMessages, setAllMessages] = useState<string[]>([]);
+
+  const roomRef = useRef<any>(null);
+  const usernameRef = useRef<any>(null);
+  const joinRoom = () => {
+    const roomVal = roomRef.current?.value;
+    const usernameVal = usernameRef.current?.value;
+    const ws = wsRef.current;
+    console.log(roomVal, usernameVal);
+    if (ws) {
+      ws.onopen = () => {
+        ws.send(
+          JSON.stringify({
+            type: "join",
+            payload: {
+              name: "ken",
+              roomId: "red",
+            },
+          })
+        );
+      };
+      ws.onmessage = (ev) => {
+        console.log(ev.data);
+        setAllMessages((mes) => [...mes, ev.data]);
+      };
+    }
+  };
+  const sendMsg = () => {
+    // @ts-ignore
+    const inputVal = inputRef.current?.value;
+    console.log(inputVal);
+    wsRef.current?.send(
+      JSON.stringify({
+        type: "chat",
+        payload: {
+          name: "ken",
+          message: `${inputVal}`,
+        },
+      })
+    );
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-between w-full max-w-200 h-full text-white">
+      <div className="flex items-center flex-col justify-center mt-18">
+        <h1 className="text-5xl font-medium font-instrument-serif italic mb-1">
+          Chit Chat
+        </h1>
+        <span className="text-md font-light text-[#acacac]">
+          one-time end-to-end encrypted anonymous chats
+        </span>
+      </div>
+      <div className="w-full max-w-100 flex flex-col items-center justify-center font-geist">
+        <span className="font-light mb-2">Join Private Chat</span>
+        <input
+          type="text"
+          ref={roomRef}
+          className="bg-[#000000] border px-4 py-3 border-[#333333] rounded-full mb-2 w-full focus-within:border-[#e0e0e0] focus-within:outline-none text-center placeholder:font-light"
+          placeholder="Enter Room Code"
+        />
+        <input
+          type="text"
+          ref={usernameRef}
+          className="bg-[#000000] border px-4 py-3 border-[#333333] rounded-full focus-within:border-[#e0e0e0] focus-within:outline-none w-full text-center placeholder:font-light"
+          placeholder="What should we call you ?"
+        />
+
+        <button
+          type="submit"
+          className="bg-white text-black w-full py-3 mt-4 rounded-full cursor-pointer hover:bg-[#e7e7e7] font-bold"
+          onClick={joinRoom}
+        >
+          Join Room
+        </button>
+
+        <span className="my-4 text-[#646464]">or</span>
+
+        <div className="w-full">
+          <button
+            type="submit"
+            className="bg-white text-black w-full py-3 rounded-full cursor-pointer hover:bg-[#e7e7e7] font-bold"
+            onClick={joinRoom}
+          >
+            Start New Private Chat
+          </button>
+        </div>
+        <div className="border border-[#333333] w-full mt-4 flex flex-col items-start justify-center p-4 rounded-lg">
+          <p>Available rooms</p>
+          <p className="text-sm font-light text-[#646464]">
+            No public rooms found. Create one or join with a code.
+          </p>
+        </div>
+      </div>
+      <p className="mb-2 text-[#646464] ">
+        messages are end-to-end encrypted and never stored
+      </p>
+    </div>
+  );
+};
+export default Home;
